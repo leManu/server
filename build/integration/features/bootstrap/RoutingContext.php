a8ecb6db053ecb9a0eb6a099aa423e255124f2d7
Côme Chilliet <come.chilliet@nextcloud.com>
2025-06-03 14:04:34 +0200

    feat(tests): Test application enabling/disabling and routes
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

---
+++ build/integration/features/bootstrap/RoutingContext.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class RoutingContext implements Context, SnippetAcceptingContext {
+	use Provisioning;
+	use AppConfiguration;
+	use CommandLine;
+
+	protected function resetAppConfigs(): void {
+	}
+}
